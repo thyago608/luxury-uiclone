6a163bf2d15f22db50aa089b6c286a346ce87302
Thyago Ribeiro <70301260+thyago608@users.noreply.github.com>
2021-05-16 00:13:21 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { Dashboard } from "./page/Dashboard";
+
+export default function App() {
+  return (
+    <>
+      <Dashboard />
+      <GlobalStyle />
+    </>
+  );
+}
